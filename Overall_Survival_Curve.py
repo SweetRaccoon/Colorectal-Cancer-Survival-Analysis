9219c0300c6a1e759af4f6696c5fe943f0da691b
Quincy <guxxx504@umn.edu>
2019-06-06 01:49:12 -0500

    Overall Survival Curve for all the colorectal cancer patients whatever their tumor recurrence or not

---
+++ Overall_Survival_Curve.py
@@ -0,0 +1,57 @@
+import csv
+from lifelines import KaplanMeierFitter
+from lifelines.statistics import logrank_test
+from pprint import pprint
+from matplotlib import pyplot as plt
+
+T = []
+E = []
+T_valid = []
+E_valid = []
+T_invalid = []
+E_invalid = []
+
+
+with open('data.csv','r') as f:
+	f_csv = csv.DictReader(f)
+	for row in f_csv:
+		if row['overall_survival'] != 'unknown':
+			T.append(int(row['overall_survival']))
+			if row['last_vital_status'] == 'Dead':
+				E.append(1)
+			else:
+				E.append(0)
+			if row['valid_varient_classification'] == 'Yes':
+				T_valid.append(T[-1])
+				E_valid.append(E[-1])
+			else:
+				T_invalid.append(T[-1])
+				E_invalid.append(E[-1])
+'''
+pprint(T)
+pprint(E)
+'''
+print('T_valid\tE_valid')
+for i in range(len(T_valid)):
+	print(T_valid[i],'\t',E_valid[i])
+
+
+results = logrank_test(T_valid, T_invalid, E_valid, E_invalid, alpha=0.95)
+results.print_summary()
+
+kmf = KaplanMeierFitter()
+
+kmf.fit(T_valid, event_observed=E_valid, label='TP53 Mutatant')
+
+kmf.survival_function_
+kmf.median_
+ax = kmf.plot(show_censors=True)
+
+
+
+
+kmf.fit(T_invalid, event_observed=E_invalid, label='TP53 Normal')
+kmf.plot(ax=ax,show_censors=True)
+plt.title('Kaplan-Meier_Overall_Survival_Curve')
+plt.show()
+
